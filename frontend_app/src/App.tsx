8bb5006e38817df35ab7a92676f1cfce3e2ec708
Kostes <qaz1234567890838@gmail.com>
2025-07-02 19:52:08 +0300

    Реализована логика авторизации и регистрации (требуется в будущем доработать)

---
+++ frontend_app/src/App.tsx
@@ -0,0 +1,12 @@
+//import {SignUpForm} from "./features/Auth/components/SignUpForm.tsx";
+import {LoginForm} from "./features/Auth/components/LoginForm.tsx";
+
+const App = () => {
+    return (
+        <>
+            <LoginForm/>
+        </>
+    )
+}
+
+export default App;
---
+++ frontend_app/src/features/Auth/type/authType.ts
@@ -0,0 +1,11 @@
+export interface RegisterType {
+    login: string,
+    email: string,
+    password: string,
+    passwordConfirm: string,
+}
+
+export interface LoginType {
+    login: string,
+    password: string,
+}
