b5cebfaeab21d62fa26700703694d6e10eedad52
Eugen <kruglikeugen@gmail.com>
2025-07-08 16:57:41 +0300

    Fourth commit: Has been does websiteMail, chat, chatMessage and added for this creaete, update, get functions

---
+++ student_recruitment_app/server/controllers/chatController.js
@@ -0,0 +1,168 @@
+const ApiError = require("../error/ApiError");
+const { User, websiteMail, Chat, ChatMessage } = require("../models");
+const { Op } = require("sequelize");
+
+class ChatController {
+    async createWebsiteMail(req, res, next) {
+        const {firstUserId, secondUserId, title, message} = req.body;
+
+        if (!firstUserId || !secondUserId || !title || !message) {
+            return next(ApiError.badRequest("Не все поля заполнены (firstUserId, secondUserId, title, message)"));
+        }
+
+        try {
+            const mail = await websiteMail.create({firstUserId, secondUserId, title, message});
+            return res.json({ mail });
+        } catch (error) {
+            console.error("Ошибка при создании websiteMail-письма:", error);
+            return next(ApiError.internal("Не удалось создать email-письмо"));
+        }
+    }
+
+    async updateWebsiteMail(req, res, next) {
+        const {id, title, message, viewed} = req.body;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID websiteMail-письма для обновления"));
+        }
+
+        try {
+            const mail = await websiteMail.findByPk(id);
+            if (!mail) return next(ApiError.notFound("WebsiteMail-письмо не найдено"));
+
+            await websiteMail.update({title, message, viewed}, {where: {id}});
+            return res.json({message: "Email-письмо обновлено"});
+        } catch (error) {
+            console.error("Ошибка при обновлении email-письма:", error);
+            return next(ApiError.internal("Не удалось обновить websiteMail-письмо"));
+        }
+    }
+
+    async getAllWebsiteMails(req, res, next) {
+        try {
+            const mails = await websiteMail.findAll();
+            return res.json({mails});
+        } catch (error) {
+            console.error("Ошибка при получении email-писем:", error);
+            return next(ApiError.internal("Не удалось получить websiteMail-письма"));
+        }
+    }
+
+
+    async createChat(req, res, next) {
+        const { usersID } = req.body;
+
+        if (!usersID || usersID.length === 0) {
+            return next(ApiError.badRequest("Не указаны пользователи для создания чата"));
+        }
+
+        try {
+            const chat = await Chat.create({usersID});
+            return res.json({chat});
+        } catch (error) {
+            console.error("Ошибка при создании чата:", error);
+            return next(ApiError.internal("Не удалось создать чат"));
+        }
+    }
+
+    async updateChat(req, res, next) {
+        const {id, usersID} = req.body;
+
+        if (!id) return next(ApiError.badRequest("Не указан ID чата для обновления"));
+
+        try {
+            const chat = await Chat.findByPk(id);
+            if (!chat) return next(ApiError.notFound("Чат не найден"));
+
+            await Chat.update({usersID}, {where: {id}});
+            return res.json({message: "Чат обновлен"});
+        } catch (error) {
+            console.error("Ошибка при обновлении чата:", error);
+            return next(ApiError.internal("Не удалось обновить чат"));
+        }
+    }
+
+    async getAllChats(req, res, next) {
+        try {
+            const chats = await Chat.findAll();
+            return res.json({chats});
+        } catch (error) {
+            console.error("Ошибка при получении чатов:", error);
+            return next(ApiError.internal("Не удалось получить чаты"));
+        }
+    }
+    
+
+    async createChatMessage(req, res, next) {
+        const {chatId, userId, message} = req.body;
+
+        if (!chatId || !userId || !message) {
+            return next(ApiError.badRequest("Не все поля заполнены (chatId, userId, message)"));
+        }
+
+        try {
+            const chatMessage = await ChatMessage.create({chatId, userId, message});
+
+            const chat = await Chat.findByPk(chatId);
+            if (!chat) {
+                return next(ApiError.notFound("Чат не найден"));
+            }
+
+            const updatedChatMessagesId = [...chat.chatMessagesId, chatMessage.id];
+
+            await Chat.update({chatMessagesId: updatedChatMessagesId }, { where: { id: chatId }});
+
+            return res.json({chatMessage});
+        } catch (error) {
+            console.error("Ошибка при создании сообщения в чате:", error);
+            return next(ApiError.internal("Не удалось создать сообщение в чате"));
+        }
+    }
+
+
+    async updateChatMessage(req, res, next) {
+        const {id, message, viewed} = req.body;
+
+        if (!id) return next(ApiError.badRequest("Не указан ID сообщения для обновления"));
+
+        try {
+            const messageObj = await ChatMessage.findByPk(id);
+            if (!messageObj) {
+                return next(ApiError.notFound("Сообщение не найдено"));
+            }
+
+            await ChatMessage.update({message, viewed}, {where: {id}});
+            return res.json({message: "Сообщение обновлено"});
+        } catch (error) {
+            console.error("Ошибка при обновлении сообщения в чате:", error);
+            return next(ApiError.internal("Не удалось обновить сообщение в чате"));
+        }
+    }
+
+    async getAllChatMessages(req, res, next) {
+    const {chatId} = req.query;
+
+    if (!chatId) {
+        return next(ApiError.badRequest("Не указан chatId"));
+    }
+
+    try {
+        const chat = await Chat.findByPk(chatId);
+        if (!chat) return next(ApiError.notFound("Чат не найден"));
+
+        const chatMessagesId = chat.chatMessagesId;
+
+        if (!chatMessagesId || chatMessagesId.length === 0) return res.json({ messages: [] });
+
+        const messages = await ChatMessage.findAll({where: {id: {[Op.in]: chatMessagesId}}});
+
+        return res.json({messages});
+    } catch (error) {
+        console.error("Ошибка при получении сообщений чата:", error);
+        return next(ApiError.internal("Не удалось получить сообщения чата"));
+    }
+}
+
+}
+
+module.exports = new ChatController();
--- student_recruitment_app/server/controllers/portfolioController.js
+++ student_recruitment_app/server/controllers/portfolioController.js
@@ -13,11 +13,11 @@ class PortfolioController {
 
         let portfolio;
         try {
-            portfolio = await Portfolio.create({ userId, salary, tegs_id: tegs, description });
+            portfolio = await Portfolio.create({userId, salary, tegs_id: tegs, description});
 
             let portfolioTeg;
             for (const teg_id of tegs) {
-                portfolioTeg = await PortfolioTeg.create({ portfolioId: portfolio.id, tegId: teg_id });
+                portfolioTeg = await PortfolioTeg.create({portfolioId: portfolio.id, tegId: teg_id});
             }
 
             return res.json({ portfolio });
@@ -28,7 +28,49 @@ class PortfolioController {
     }
 
     async updatePortfolio(req, res, next) {
+        const { id, userId, salary, tegs, description } = req.body;
 
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID портфолио для обновления"));
+        }
+
+        try {
+            const portfolio = await Portfolio.findByPk(id);
+            if (!portfolio) {
+                return next(ApiError.notFound("Портфолио не найдено"));
+            }
+
+            const updatedPortfolio = await Portfolio.update(
+                {userId, salary, tegs_id: tegs, description},
+                {where: {id}}
+            );
+
+            return res.json({ updatedPortfolio });
+        } catch (error) {
+            console.error("Ошибка при обновлении портфолио:", error);
+            return next(ApiError.internal("Не удалось обновить портфолио"));
+        }
+    }
+
+    async deletePortfolio(req, res, next) {
+        const {id} = req.params;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID портфолио для удаления"));
+        }
+
+        try {
+            const portfolio = await Portfolio.findByPk(id);
+            if (!portfolio) {
+                return next(ApiError.notFound("Портфолио не найдено"));
+            }
+
+            await Portfolio.destroy({ where: { id } });
+            return res.json({ message: "Портфолио успешно удалено" });
+        } catch (error) {
+            console.error("Ошибка при удалении портфолио:", error);
+            return next(ApiError.internal("Не удалось удалить портфолио"));
+        }
     }
 
     async getAllPortfolio(req, res, next) {
@@ -41,11 +83,11 @@ class PortfolioController {
 
         let whereConditions = {};
 
-        if (min_salary) whereConditions.salary = { [Op.gte]: min_salary };
+        if (min_salary) whereConditions.salary = {[Op.gte]: min_salary};
 
-        if (max_salary) whereConditions.salary = { ...whereConditions.salary, [Op.lte]: max_salary };
+        if (max_salary) whereConditions.salary = {...whereConditions.salary, [Op.lte]: max_salary};
 
-        if (tegs && tegs.length > 0) whereConditions.tegs_id = { [Op.overlap]: tegs };
+        if (tegs && tegs.length > 0) whereConditions.tegs_id = {[Op.overlap]: tegs};
 
         console.log("Условия запроса:", whereConditions);
 
@@ -69,6 +111,53 @@ class PortfolioController {
         return res.json({teg});
     }
 
+    async updateTeg(req, res, next) {
+        const { id, name } = req.body;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID тега для обновления"));
+        }
+
+        try {
+            const teg = await Teg.findByPk(id);
+            if (!teg) {
+                return next(ApiError.notFound("Тег не найден"));
+            }
+
+            const updatedTeg = await Teg.update(
+                {name},
+                {where: {id}}
+            );
+
+            return res.json({ updatedTeg });
+        } catch (error) {
+            console.error("Ошибка при обновлении тега:", error);
+            return next(ApiError.internal("Не удалось обновить тег"));
+        }
+    }
+
+    async deleteTeg(req, res, next) {
+        const {id} = req.params;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID тега для удаления"));
+        }
+
+        try {
+            const teg = await Teg.findByPk(id);
+            if (!teg) {
+                return next(ApiError.notFound("Тег не найден"));
+            }
+
+            await Teg.destroy({ where: {id} });
+            return res.json({ message: "Тег успешно удален" });
+        } catch (error) {
+            console.error("Ошибка при удалении тега:", error);
+            return next(ApiError.internal("Не удалось удалить тег"));
+        }
+    }
+
+
     async getAllTegs(req, res, next) {
         const tegs = await Teg.findAll();
 
@@ -86,12 +175,68 @@ class PortfolioController {
 
 
     async updateProject(req, res, next) {
-        
+        const {id, name, description, images, link} = req.body;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID проекта для обновления"));
+        }
+
+        try {
+            const project = await Project.findByPk(id);
+            if (!project) {
+                return next(ApiError.notFound("Проект не найден"));
+            }
+
+            const updatedProject = await Project.update(
+                { name, description, images, link },
+                { where: { id } }
+            );
+
+            return res.json({ updatedProject });
+        } catch (error) {
+            console.error("Ошибка при обновлении проекта:", error);
+            return next(ApiError.internal("Не удалось обновить проект"));
+        }
     }
 
-    async getAllProjects(req, res, next) {
-        
+    async deleteProject(req, res, next) {
+        const {id} = req.params;
+
+        if (!id) {
+            return next(ApiError.badRequest("Не указан ID проекта для удаления"));
+        }
+
+        try {
+            const project = await Project.findByPk(id);
+            if (!project) {
+                return next(ApiError.notFound("Проект не найден"));
+            }
+
+            await Project.destroy({ where: {id} });
+            return res.json({ message: "Проект успешно удален" });
+        } catch (error) {
+            console.error("Ошибка при удалении проекта:", error);
+            return next(ApiError.internal("Не удалось удалить проект"));
+        }
     }
+
+
+    async getAllProjects(req, res, next) {
+        let {limit, page} = req.body;
+
+        limit = limit || 4;
+        page = page || 1;
+        const offset = (page - 1) * limit;
+
+        try {
+            const projects = await Project.findAll({ limit, offset });
+            return res.json(projects);
+        } catch (error) {
+            console.error("Ошибка при получении проектов:", error);
+            return next(ApiError.internal("Не удалось получить проекты"));
+        }
+    }
+
 }
 
 module.exports = new PortfolioController()
